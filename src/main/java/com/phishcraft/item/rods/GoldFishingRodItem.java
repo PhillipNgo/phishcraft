07155b7ecbc3d665e4d834ca73767b131239b536
Lauren Levine <lrlevine7@gmail.com>
2023-05-28 19:57:19 -0400

    Add blaze, emerald, gold and obsidian fishing rods

---
+++ src/main/java/com/phishcraft/item/rods/GoldFishingRodItem.java
@@ -0,0 +1,11 @@
+package com.phishcraft.item.rods;
+
+import net.minecraft.world.item.FishingRodItem;
+import net.minecraft.world.item.Item;
+
+public class GoldFishingRodItem extends FishingRodItem {
+
+  public GoldFishingRodItem() {
+    super(new Item.Properties());
+  }
+}
---
+++ src/main/java/com/phishcraft/item/rods/ObsidianFishingRodItem.java
@@ -0,0 +1,11 @@
+package com.phishcraft.item.rods;
+
+import net.minecraft.world.item.FishingRodItem;
+import net.minecraft.world.item.Item;
+
+public class ObsidianFishingRodItem extends FishingRodItem {
+
+  public ObsidianFishingRodItem() {
+    super(new Item.Properties());
+  }
+}
