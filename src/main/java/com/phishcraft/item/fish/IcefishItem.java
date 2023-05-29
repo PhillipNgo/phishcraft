75c0c45420b8b8a44300bf2f4132d839524d26da
PhillipNgo <phllpqng@gmail.com>
2023-05-29 11:20:42 -0700

    Eating icefish clears burning

--- src/main/java/com/phishcraft/item/fish/IcefishItem.java
+++ src/main/java/com/phishcraft/item/fish/IcefishItem.java
@@ -1,16 +1,25 @@
 package com.phishcraft.item.fish;
 
+import com.phishcraft.item.food.FoodItem;
+
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.food.Foods;
 import net.minecraft.world.item.Item;
 
-public class IcefishItem extends Item {
+public class IcefishItem extends FoodItem {
 
   public IcefishItem() {
     super((new Item.Properties()).food(
         (new FoodProperties.Builder()
+            .alwaysEat()
             .saturationMod(Foods.COD.getSaturationModifier())
             .nutrition(Foods.COD.getNutrition())
             .build())));
+  }
+
+  @Override
+  public void onFoodEaten(Player player) {
+    player.setRemainingFireTicks(0);
   }
 }
