d0edb5c97720dea27285aef6ff1f0a127711cbc9
lrlevine7 <134811991+lrlevine7@users.noreply.github.com>
2023-05-29 00:38:35 -0400

    Boba (#9)
    * Add diamond and netherite fishing rods.

---
+++ src/main/java/com/phishcraft/item/rods/NetheriteFishingRodItem.java
@@ -0,0 +1,11 @@
+package com.phishcraft.item.rods;
+
+import net.minecraft.world.item.FishingRodItem;
+import net.minecraft.world.item.Item;
+
+public class NetheriteFishingRodItem extends FishingRodItem {
+
+  public NetheriteFishingRodItem() {
+    super(new Item.Properties());
+  }
+}
